f62e7fda1e7eed98a4df85c7baebe836d9578b8f
Norlon <nsibug@flemingcollege.ca>
2025-01-14 19:49:24 -0500

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 # Print lines from Zen of Python
 print("There should be one-- and preferably only one --obvious way to do it.")
 
 # This is an amazing new feature.
+=======
+>>>>>>> parent of 10f7d9a (Update script1.py)
