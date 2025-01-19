ad66a989350b0e31b1150101f375ed503729cc95
Norlon <nsibug@flemingcollege.ca>
2025-01-19 15:02:41 -0500

    update script

--- script1.py
+++ script1.py
@@ -1,5 +1,9 @@
 # Print lines from Zen of Python
 print("There should be one-- and preferably only one --obvious way to do it.")
+<<<<<<< HEAD
 print("Although that way may not be obvious at first unless you're Dutch")
 
 
+=======
+print("Although that way may not be obvious at first unless you're Dutch.")
+>>>>>>> parent of e561c6b (Completed new feature)
